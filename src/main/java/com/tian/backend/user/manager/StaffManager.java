56cbc2c5177170e7911a88da5bf338c59adeea83
muyang.tian <tmy1117@163.com>
2021-04-25 11:21:13 +0800

    first

--- src/main/java/com/tian/backend/user/manager/StaffManager.java
+++ src/main/java/com/tian/backend/user/manager/StaffManager.java
@@ -1,10 +1,13 @@
 package com.tian.backend.user.manager;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tian.backend.user.common.CommonEnum;
 import com.tian.backend.user.model.Staff;
 import com.tian.backend.user.repository.StaffRepository;
 import com.tian.backend.user.service.StaffService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 
@@ -15,15 +18,13 @@ import java.util.List;
  * @author muyang.tian
  * @date 2021/4/19 10:53
  */
+@Slf4j
 @Service
-public class StaffManager implements StaffService {
-
-    @Resource
-    private StaffRepository baseMapper;
+public class StaffManager extends ServiceImpl<StaffRepository,Staff> implements StaffService {
 
     @Override
-    public List<Staff> index() {
-        return baseMapper.selectList(null);
+    public Page<Staff> page(Page<Staff> pageRequest, String keywords, String state) {
+        return baseMapper.page(pageRequest,keywords,state);
     }
 
     @Override
@@ -38,13 +39,8 @@ public class StaffManager implements StaffService {
     }
 
     @Override
-    public Staff getById(Long id) {
-        return baseMapper.selectOne(new LambdaQueryWrapper<Staff>().eq(Staff::getId,id));
-    }
-
-    @Override
-    public Staff update(Staff updating) {
+    public Staff update(Long id,Staff updating) {
         baseMapper.updateById(updating);
-        return baseMapper.selectOne(new LambdaQueryWrapper<Staff>().eq(Staff::getLoginName,updating.getLoginName()));
+        return baseMapper.selectOne(new LambdaQueryWrapper<Staff>().eq(Staff::getId,id));
     }
 }
--- src/test/java/com/tian/backend/BackendApplicationTests.java
+++ src/test/java/com/tian/backend/BackendApplicationTests.java
@@ -1,13 +1,27 @@
 package com.tian.backend;
 
+import cn.hutool.extra.pinyin.PinyinUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        String[] p = new String[]{"IV","奥凯","百年","百威","博优","常春藤","畅捷通","超旺",
+                "超赢","管家婆","海典","浩普","皓客","华创","汇云朗翰","经营圣手","凯宝","科脉"
+                ,"科脉蛙笑V11","上海海典","时空智友","思讯-eshop4","思讯-eshop5","思讯-eshop6"
+                ,"思讯-爱贝","思讯-爱客","思讯-孕婴童3","思讯-专卖10","思讯-专卖7","思讯-专卖8",
+                "思讯-专卖9","泰格","童梦国","旺盛","西联","赢通","用友","智百威","紫日"};
+        List<String> list = new ArrayList<>();
+        for (String a: p) {
+            list.add(PinyinUtil.getPinyin(a,""));
+        }
+        list.forEach(System.out::println);
     }
 
 }
