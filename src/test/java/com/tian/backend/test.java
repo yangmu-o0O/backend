f6a7ff3e03ad96a517822b2b090801a871091b10
muyang.tian <tmy1117@163.com>
2021-05-25 17:49:01 +0800

    不需要提交的文件

---
+++ src/test/java/com/tian/backend/test.java
@@ -0,0 +1,16 @@
+package com.tian.backend;
+
+/**
+ * @author muyang.tian
+ * @date 2021/5/24 18:25
+ */
+public enum test {
+    aa,
+    ee,
+    cc,
+    qq;
+
+    public static void main(String[] args) {
+        System.out.println(test.values()[1]);
+    }
+}
